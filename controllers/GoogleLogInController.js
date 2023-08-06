d54b232b1dc7e249ecefbfc0f823e99a1f2d5d82
majdiShomali <majdi.shomali.1997@gmail.com>
2023-08-06 13:31:36 +0300

    tttttt

--- controllers/GoogleLogInController.js
+++ controllers/GoogleLogInController.js
@@ -4,45 +4,47 @@ const jwt = require("jsonwebtoken");
 const SECRETKEY = process.env.SECRETKEY;
 
 const newUserGoogle = async (req, res) => {
-  console.log("New user")
-  res.status(500).json({ message: "incorrect password" });
-  // const { name, email, picture, verified_email, id } = req.body;
-  // const userExist = await User.find({ email: email });
+  const { name, email, picture, verified_email, id } = req.body;
+  const userExist = await User.find({ email: email });
+console.log("11111111111")
+  if (userExist.length == 0) {
+    console.log("22222222222")
+    const hashPassword = await bcrypt.hash(id, 5);
+    const NewUser = new User({
+      userName: name,
+      email: email,
+      password: hashPassword,
+      role: 0,
+    });
+    const user = await NewUser.save();
+    const token = jwt.sign(
+      { id: user._id, userName: user.userName, role: user.role },
+      SECRETKEY,
+      { expiresIn: "24h" }
+    );
+    return res.status(200).json({ token, user });
 
-  // if (userExist.length == 0) {
-  //   const hashPassword = await bcrypt.hash(id, 5);
-  //   const NewUser = new User({
-  //     userName: name,
-  //     email: email,
-  //     password: hashPassword,
-  //     role: 0,
-  //   });
-  //   const user = await NewUser.save();
-  //   const token = jwt.sign(
-  //     { id: user._id, userName: user.userName, role: user.role },
-  //     SECRETKEY,
-  //     { expiresIn: "24h" }
-  //   );
-  //   return res.status(200).json({ token, user });
+  } else {
+    console.log("333333333")
 
-  // } else {
-  //   const validpassword = await bcrypt.compare(id, userExist[0].password);
+    const validpassword = await bcrypt.compare(id, userExist[0].password);
 
-  //   if (!validpassword) {
-  //     return res.status(500).json({ message: "incorrect password" });
-  //   }
+    if (!validpassword) {
+      return res.status(500).json({ message: "incorrect password" });
+    }
 
-  //   if (validpassword) {
-  //     const token = jwt.sign(
-  //       { id: userExist[0]._id, userName: userExist[0].userName, role: userExist[0].role },
-  //       SECRETKEY,
-  //       { expiresIn: "24h" }
-  //     );
-  //     const user = userExist[0];
-  //     return res.status(200).json({ token, user });
-  //   }
-  // }
+    if (validpassword) {
+      const token = jwt.sign(
+        { id: userExist[0]._id, userName: userExist[0].userName, role: userExist[0].role },
+        SECRETKEY,
+        { expiresIn: "24h" }
+      );
+      const user = userExist[0];
+      return res.status(200).json({ token, user });
+    }
+  }
 
+  
 };
 
 
